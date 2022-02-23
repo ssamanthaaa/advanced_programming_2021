f923c9f760cc53dd6e21ffe72edba4bcbbc262e3
Kevin Marzio <marzio92@gmail.com>
2022-02-23 09:06:16 +0100

    Add new_stack() and free_stack(x)

--- exam/stack_pool.hpp
+++ exam/stack_pool.hpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
 
-template <typename SP, typename ST, typename V> 
+template <typename SP, typename ST, typename V>
 class _iterator {
   SP* pool;
-  ST current; // current head
-public:
+  ST current;  // current head
+ public:
   using value_type = V;
   using stack_type = ST;
   using stack_pool = SP;
@@ -13,17 +13,17 @@ public:
   using pointer = value_type*;
   using difference_type = std::ptrdiff_t;
   using iterator_category = std::forward_iterator_tag;
-  
+
   explicit _iterator(stack_pool* p, stack_type head) : pool{p}, current{head} {}
-  
+
   reference operator*() const noexcept { return pool->value(current); }
 
-  _iterator& operator++() { // pre-increment
+  _iterator& operator++() {  // pre-increment
     current = pool->next(current);
     return *this;
   }
 
-  _iterator operator++(int) { // post_increment
+  _iterator operator++(int) {  // post_increment
     auto tmp = *this;
     ++(*this);
     return tmp;
@@ -47,7 +47,7 @@ class stack_pool {
     node_t() = default;
 
     // should I check next>=0 ?
-    explicit node_t(T value, N next): value{value}, next{next} {};
+    explicit node_t(T value, N next) : value{value}, next{next} {};
 
     ~node_t() = default;
   };
@@ -62,21 +62,23 @@ class stack_pool {
 
   /**
    * Function to push a new value in the stack
-   * If the first empty node is the last then we create a new node, we used emplace_back so the object is constructed directly
-   * If the first empty node is not the last this means that we have some free space somewhere.
+   * If the first empty node is the last then we create a new node, we used
+   * emplace_back so the object is constructed directly If the first empty node
+   * is not the last this means that we have some free space somewhere.
    */
   template <typename X>
   stack_type _push(X&& val, stack_type head) {
     // I have to push into the vector a new node
     if (empty(free_nodes)) {
       pool.emplace_back(std::forward<X>(val), head);
-      return static_cast<stack_type>(pool.size());// next free node is the last
-    } 
-    else { // we have a free space somewhere
-      stack_type new_head = free_nodes; 
-      free_nodes = next(free_nodes); // we update free_nodes with the next position
-      value(new_head) =std::forward<X>(val);
-      next(new_head) = head; // the next of the new_head is the previous head
+      return static_cast<stack_type>(
+          pool.size());  // next free node is the last
+    } else {             // we have a free space somewhere
+      stack_type new_head = free_nodes;
+      free_nodes =
+          next(free_nodes);  // we update free_nodes with the next position
+      value(new_head) = std::forward<X>(val);
+      next(new_head) = head;  // the next of the new_head is the previous head
       return new_head;
     }
   }
@@ -84,7 +86,7 @@ class stack_pool {
  public:
   stack_pool() : pool{}, free_nodes{end()} {};
 
-  explicit stack_pool(size_type n): stack_pool{} {
+  explicit stack_pool(size_type n) : stack_pool{} {
     reserve(n);
   };  // reserve n nodes in the pool
 
@@ -102,18 +104,21 @@ class stack_pool {
   const_iterator cbegin(stack_type x) const;
   const_iterator cend(stack_type) const;
 
-  stack_type new_stack();  // return an empty stack
+  /**
+   * Create an empty stack. Initial value is (std::size_t(0)).
+   */
+  stack_type new_stack() noexcept { return end(); };  // return an empty stack
 
-  void reserve(size_type n){
+  void reserve(size_type n) {
     // what if n<0 or n<capacity ??
     pool.reserve(n);
-  };   // reserve n nodes in the pool
+  };  // reserve n nodes in the pool
 
-  size_type capacity() const { // the capacity of the pool
+  size_type capacity() const {  // the capacity of the pool
     // return pool.capacity();
   }
 
-  bool empty(stack_type x) const { // forse noexcept
+  bool empty(stack_type x) const {  // forse noexcept
     return x == end();
   }
 
@@ -125,15 +130,13 @@ class stack_pool {
   stack_type& next(stack_type x) { return node(x).next; }
   const stack_type& next(stack_type x) const { return node(x).next; }
 
-  stack_type push(const T& val, stack_type head) {
-    return _push(val, head);
-  }
+  stack_type push(const T& val, stack_type head) { return _push(val, head); }
   stack_type push(T&& val, stack_type head) {
     return _push(std::move(val), head);
   }
 
-  stack_type pop(stack_type x){
-    // what if x is empty or does not exists?
+  stack_type pop(stack_type x) {
+    // what if x is empty, does not exists or it is free_nodes?
     if (empty(x)) {
       return end();
     } else {
@@ -144,5 +147,15 @@ class stack_pool {
     }
   };  // delete first node
 
-  stack_type free_stack(stack_type x);  // free entire stack
+  /**
+   * Function to free a single stack. The stack is resetted to its initial
+   * value (std::size_t(0)). Its nodes are added to free_nodes.
+   */
+  stack_type free_stack(stack_type x) {
+    // what if x does not exist or it is free nodes?
+    while (!empty(x)) {
+      x = this->pop(x);
+    }
+    return (x);
+  };  // free entire stack
 };
