e20f6c1d213bd967810d51831457daed4b2b73e7
Kevin Marzio <marzio92@gmail.com>
2022-02-24 12:47:43 +0100

    Add some ERROR to check user inputs

--- exam/stack_pool.hpp
+++ exam/stack_pool.hpp
@@ -1,15 +1,28 @@
+#include "ap_error.hpp"
 #include <iostream>
 #include <vector>
-#include "ap_error.hpp"
+
+#define AP_ERROR_CUSTOM_POSITIVE(a)                                            \
+  AP_ERROR(((a >= end()) && (a <= stack_type{pool.size()})), Invalid_input)    \
+      << "The stack must be >= " << end() << " and <= " << pool.size() << "."  \
+      << "\n"                                                                  \
+      << "You have passed: " << a                                              \
+      << " (the value shown is casted in stack_type)." << std::endl;
+
+#define AP_ERROR_CUSTOM_STRICT_POSITIVE(a)                                     \
+  AP_ERROR(((a > end()) && (a <= stack_type{pool.size()})), Invalid_input)     \
+      << "The stack must be >= " << end() << " and <= " << pool.size() << "."  \
+      << "\n"                                                                  \
+      << "You have passed: " << a                                              \
+      << " (the value shown is casted in stack_type)." << std::endl;
 
 /**
  * @brief A simple class to handle wrong inputs
  */
-struct Invalind_input: public std::runtime_error{
-  using std::runtime_error::runtime_error; // using the same constructors
-                                           // of the parent
+struct Invalind_input : public std::runtime_error {
+  using std::runtime_error::runtime_error;  // using the same constructors
+                                            // of the parent
 };
-
 
 template <typename SP, typename ST, typename V>
 class _iterator {
@@ -73,18 +86,21 @@ class stack_pool {
 
   /**
    * @brief Function to push a new value in the stack
-   * 
-   * If the first empty node is the last then we create a new node directly inside our pool with emplace_back
-   * If the frist empty node is not the last this means that we have some free space somewhere, so there is no need to create a node.
+   *
+   * If the first empty node is the last then we create a new node directly
+   * inside our pool with emplace_back If the frist empty node is not the last
+   * this means that we have some free space somewhere, so there is no need to
+   * create a node.
    */
   template <typename X>
   stack_type _push(X&& val, stack_type head) {
     if (empty(free_nodes)) {
       pool.emplace_back(std::forward<X>(val), head);
-      return static_cast<stack_type>(pool.size()); 
+      return static_cast<stack_type>(pool.size());
     } else {
       stack_type new_head = free_nodes;
-      free_nodes = next(free_nodes);  // we update free_nodes with the next position
+      free_nodes =
+          next(free_nodes);  // we update free_nodes with the next position
       value(new_head) = std::forward<X>(val);
       next(new_head) = head;  // the next of the new_head is the previous head
       return new_head;
@@ -109,7 +125,6 @@ class stack_pool {
   using iterator = _iterator<stack_pool, stack_type, value_type>;
   using const_iterator = _iterator<stack_pool, stack_type, const value_type>;
 
-
   /**
    * @brief Returning an iterator to the beginning of the stack.
    */
@@ -117,25 +132,27 @@ class stack_pool {
 
   /**
    * @brief Returning an iterator to the end of the stack
-   * 
+   *
    */
-  iterator end(stack_type) { return iterator(this, end()); } // this is not a typo
+  iterator end(stack_type) {
+    return iterator(this, end());
+  }  // this is not a typo
 
   /**
    * @brief Returning a const_iterator to the beginning of the stack.
    */
   const_iterator begin(stack_type x) const { return const_iterator(this, x); }
-  
+
   /**
    * @brief Returning a const_iterator to the end of the stack.
    */
-  const_iterator end(stack_type) const  { return const_iterator(this, end()); }
+  const_iterator end(stack_type) const { return const_iterator(this, end()); }
 
   /**
    * @brief Returning a const_iterator to the beginning of the stack.
    */
   const_iterator cbegin(stack_type x) const { return const_iterator(this, x); }
-  
+
   /**
    * @brief Returning a const_iterator to the end of the stack.
    */
@@ -146,16 +163,14 @@ class stack_pool {
    */
   stack_type new_stack() noexcept { return end(); };  // return an empty stack
 
-
   /**
    * @brief A function that reserves n nodes in the pool
    */
   void reserve(size_type n) {
-    AP_ASSERT_GT(n,0);
+    AP_ASSERT_GT(n, 0);
     AP_ASSERT_GT(n, capacity());
     pool.reserve(n);
   };  // reserve n nodes in the pool
-
 
   /**
    * @brief Gives the capacity of the pool
@@ -168,91 +183,109 @@ class stack_pool {
    * @brief Checking if the given stack is empty or not
    */
   bool empty(stack_type x) const {  // forse noexcept
+    AP_ERROR_CUSTOM_POSITIVE(x);
     return x == end();
   }
 
   /**
-   * @brief Returning the value 0, which is the index of the last node in the vector. 
+   * @brief Returning the value 0, which is the index of the last node in the
+   * vector.
    */
   stack_type end() const noexcept { return stack_type(0); }
 
-  
   /**
-   * @brief Returning a reference to the value of the node. 
-  */
-  T& value(stack_type x) { return node(x).value; }
+   * @brief Returning a reference to the value of the node.
+   */
+  T& value(stack_type x) {
+    AP_ERROR_CUSTOM_STRICT_POSITIVE(x);
+    return node(x).value;
+  }
 
   /**
    * @brief Returning a const reference to the value of the node.
    */
-  const T& value(stack_type x) const { return node(x).value; }
+  const T& value(stack_type x) const {
+    AP_ERROR_CUSTOM_STRICT_POSITIVE(x);
+    return node(x).value;
+  }
 
   /**
-   * @brief Returning a reference to the next node of the node x. 
-  */
-  stack_type& next(stack_type x) { return node(x).next; }
+   * @brief Returning a reference to the next node of the node x.
+   */
+  stack_type& next(stack_type x) {
+    AP_ERROR_CUSTOM_STRICT_POSITIVE(x);
+    return node(x).next;
+  }
 
   /**
    * @brief Returning a const reference to the next node of the node x
    */
-  const stack_type& next(stack_type x) const { return node(x).next; }
+  const stack_type& next(stack_type x) const {
+    AP_ERROR_CUSTOM_STRICT_POSITIVE(x);
+    return node(x).next;
+  }
 
   /**
    * @brief Function to push an element to the front of the stack
    */
-  stack_type push(const T& val, stack_type head) { return _push(val, head); }
+  stack_type push(const T& val, stack_type head) {
+    AP_ERROR_CUSTOM_POSITIVE(head);
+    return _push(val, head);
+  }
   stack_type push(T&& val, stack_type head) {
+    AP_ERROR_CUSTOM_POSITIVE(head);
     return _push(std::move(val), head);
   }
 
   /**
-   * @brief Function to remove from the stack the last inserted node, that will be put in the free_nodes
-  */
+   * @brief Function to remove from the stack the last inserted node, that will
+   * be put in the free_nodes
+   */
   stack_type pop(stack_type x) {
-    // what if x is empty, does not exists or it is free_nodes?
-    if (empty(x)) {
-      return end();
-    } else {
-      auto new_head = next(x);
-      next(x) = free_nodes;
-      free_nodes = x;
-      return new_head;
-    }
+    AP_ERROR_CUSTOM_STRICT_POSITIVE(x);
+    AP_ERROR(x == free_nodes, Invalid_input)
+        << "You can not pop from the stack of free_nodes" << std::endl;
+    auto new_head = next(x);
+    next(x) = free_nodes;
+    free_nodes = x;
+    return new_head;
   }  // delete first node
 
   /**
-   * @brief Function to free a single stack. The stack is resetted to its initial
-   * value (std::size_t(0)). Its nodes are added to free_nodes.
+   * @brief Function to free a single stack. The stack is resetted to its
+   * initial value (std::size_t(0)). Its nodes are added to free_nodes.
    */
   stack_type free_stack(stack_type x) {
-    // what if x does not exist or it is free nodes?
+    AP_ERROR_CUSTOM_STRICT_POSITIVE(x);
+    AP_ERROR(x == free_nodes, Invalid_input)
+        << "You can not free the stack of free_nodes" << std::endl;
     while (!empty(x)) {
       x = this->pop(x);
     }
     return (x);
-  } // free entire stack
-
+  }  // free entire stack
 
   /**
    * @brief Function to print a single stack
-  */
+   */
   void print_stack(const stack_type x) {
+    AP_ERROR_CUSTOM_POSITIVE(x);
     auto start = begin(x);
     std::cout << "[ ";
     while (start != end(x)) {
-        std::cout << *start << " ";
-        ++start;
+      std::cout << *start << " ";
+      ++start;
     }
     std::cout << "]" << std::endl;
   }
 
   /**
    * @brief Function to print the content of the pool
-  */
+   */
   void print_pool() {
     std::cout << "pool = [ ";
     for (auto i = 0; i < pool.size(); ++i) {
-        std::cout << pool.at(i).value << " ";
+      std::cout << pool.at(i).value << " ";
     }
     std::cout << "]" << std::endl;
   }
